28c78fe54765fadbe5f7b9f94f3357bcb3e8b5f7
unknown <Admin@ss940399351.redmond.corp.microsoft.com>
2014-01-08 10:40:13 -0500

    added pid

---
+++ src/controller/PID.java
@@ -0,0 +1,41 @@
+package controller;
+
+public class PID {
+	private final double proportionalC;
+	private final double derivativeC;
+	private final double integralC;
+	private final double desired;
+	
+	private double integralError;
+	private double previousError;
+	private double previousTime;
+	
+	/*
+	 * integralC = 0
+	 * dervativeC = 0
+	 * proportionalC = underestimate
+	 */
+	public PID (double desired, double proportionalC, double derivativeC, double integralC) {
+		this.proportionalC = proportionalC;
+		this.derivativeC = derivativeC;
+		this.desired = desired;
+		this.integralC = integralC;
+		this.integralError = 0;
+		this.previousError = 0;
+		this.previousTime = System.currentTimeMillis();
+	}
+	
+	public double update(double actual) {
+		double time = System.currentTimeMillis();
+		double error = desired-actual;
+		double proportional = proportionalC*(error);
+		integralError += integralC*error*(time-previousTime);
+		double derivative = derivativeC*(error-previousError)/(time-previousTime);
+		previousTime = time;
+		previousError = error;
+		
+		return proportional + integralError + derivative;
+	}
+	
+	public double update
+}
